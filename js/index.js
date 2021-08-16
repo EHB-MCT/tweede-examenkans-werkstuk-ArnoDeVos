132b662311fa1e07f8d8b2dc3f7e83b19a78db37
Arno De Vos <arno.de.vos@student.ehb.be>
2021-08-16 15:25:59 +0200

    adding scripts, eerste aanzet code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 "use strict";
+
+getData();
+
+function getData() {
+    axios.get('https://thecrew.cc/news/read.php')
+        .then(function (response) {
+            let data = response.data.news;
+            load(data);
+        });
+}
+
+function load(data) {
+    let html = "";
+    data.forEach(elem => {
+        html += `<div class="card m-2" style="width: 18rem; float:left; padding:5px">
+        <img class="card-img-top" src="${elem.imageURI}" alt="Card image cap" />
+        </div>`;
+    });
+    document.getElementById('html').innerHTML = html;
+}
