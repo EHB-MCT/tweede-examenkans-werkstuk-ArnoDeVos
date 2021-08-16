666ef06d2d51664eb46516909fe7bfd922aa4bd6
Arno De Vos <arno.de.vos@student.ehb.be>
2021-08-16 16:26:05 +0200

    fix text

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ function load(data) {
         <img class="card-img-top" src="${elem.imageURI}" alt="Card image cap" />
         <div class="card-body">
         <h5 class="card-title ml-0">${elem.title}</h5>
-        <p class="card-text">${elem.content.split('</p>')[0].replace('<p>', "")}</p>
+        <p class="card-text">${elem.content.split('</p>')[0].replace('<p>', "").replace('</p>', "").replace('<strong>', "").replace('</strong>', "").replace('<h4>', "").replace('</h4>', "").replace('<strong>', "").replace('</strong>', "").replace('<strong>', "").replace('</strong>', "")}</p>
     </div>
         </div>`;
     });
