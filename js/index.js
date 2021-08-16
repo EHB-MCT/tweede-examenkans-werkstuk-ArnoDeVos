33088d102300f7512f0f4dce4359bbc99ec59615
Arno De Vos <arno.de.vos@student.ehb.be>
2021-08-16 23:26:47 +0200

    layout fixes

--- js/index.js
+++ js/index.js
@@ -13,11 +13,11 @@ function getData() {
 function load(data) {
     let html = "";
     data.forEach(elem => {
-        html += `<div class="card m-2" style="width: 18rem; float:left; padding:5px">
+        html += `<div class="card m-2" style="width: 25rem; float:left; padding:5px">
         <img class="card-img-top" src="${elem.imageURI}" alt="Card image cap" />
         <div class="card-body">
-        <h5 class="card-title ml-0">${elem.title}</h5>
-        <p class="card-text">${elem.content.split('</p>')[0].replace('<p>', "").replace('</p>', "").replace('<strong>', "").replace('</strong>', "").replace('<h4>', "").replace('</h4>', "").replace('<strong>', "").replace('</strong>', "").replace('<strong>', "").replace('</strong>', "")}</p>
+        <h5 class="card-title" style="height:3.5em;">${elem.title}</h5>
+        <p class="card-text cut-text">${elem.content.split('</p>')[0].replace('<p>', "").replace('</p>', "").replace('<strong>', "").replace('</strong>', "").replace('<h4>', "").replace('</h4>', "").replace('<strong>', "").replace('</strong>', "").replace('<strong>', "").replace('</strong>', "")}</p>
         <p class="Datum">Datum: ${elem.publicationDate}</p>
         <a href="${elem.linkTo}" target="_blank" class="btn btn-outline-primary btn-sm" style='float:left'>Lees meer</a>
         <i class="fa fa-heart fa-lg" aria-hidden="true" onclick="like('${elem.UUID}')"></i> ${elem.likes}
