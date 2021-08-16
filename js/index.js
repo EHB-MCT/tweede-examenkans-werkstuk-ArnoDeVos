f08412b787ea2525fded9490c6622b80bb89293b
Arno De Vos <arno.de.vos@student.ehb.be>
2021-08-16 17:31:38 +0200

    like function

--- js/index.js
+++ js/index.js
@@ -19,10 +19,18 @@ function load(data) {
         <h5 class="card-title ml-0">${elem.title}</h5>
         <p class="card-text">${elem.content.split('</p>')[0].replace('<p>', "").replace('</p>', "").replace('<strong>', "").replace('</strong>', "").replace('<h4>', "").replace('</h4>', "").replace('<strong>', "").replace('</strong>', "").replace('<strong>', "").replace('</strong>', "")}</p>
         <a href="${elem.linkTo}" target="_blank" class="btn btn-outline-primary btn-sm" style='float:left'>Lees meer</a>
-        <i class="fa fa-heart fa-lg" aria-hidden="true"></i> ${elem.likes}
+        <i class="fa fa-heart fa-lg" aria-hidden="true" onclick="like('${elem.UUID}')"></i> ${elem.likes}
         </div>
         </div>`;
     });
     document.getElementById('html').innerHTML = html;
 }
 
+function like(id) {
+    axios.post('https://thecrew.cc/news/create.php', {
+        UUID: id,
+    })
+        .then(function (response) {
+            getData();
+        });
+}
