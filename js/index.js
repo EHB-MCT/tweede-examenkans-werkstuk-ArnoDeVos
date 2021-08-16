9283bdb589884b4789dd10b7b492b0bd5c237348
Arno De Vos <arno.de.vos@student.ehb.be>
2021-08-16 16:16:18 +0200

    Shorter text

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ function load(data) {
         <img class="card-img-top" src="${elem.imageURI}" alt="Card image cap" />
         <div class="card-body">
         <h5 class="card-title ml-0">${elem.title}</h5>
-        <p class="card-text">${elem.content}</p>
+        <p class="card-text">${elem.content.split('</p>')[0].replace('<p>', "")}</p>
     </div>
         </div>`;
     });
