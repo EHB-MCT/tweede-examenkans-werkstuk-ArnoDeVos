14a14baa7709cc1a3f4b5df16a9edeac5dd10208
Arno De Vos <arno.de.vos@student.ehb.be>
2021-08-16 16:42:25 +0200

    added href

--- js/index.js
+++ js/index.js
@@ -18,8 +18,10 @@ function load(data) {
         <div class="card-body">
         <h5 class="card-title ml-0">${elem.title}</h5>
         <p class="card-text">${elem.content.split('</p>')[0].replace('<p>', "").replace('</p>', "").replace('<strong>', "").replace('</strong>', "").replace('<h4>', "").replace('</h4>', "").replace('<strong>', "").replace('</strong>', "").replace('<strong>', "").replace('</strong>', "")}</p>
-    </div>
+        <a href="${elem.linkTo}" target="_blank" class="btn btn-outline-primary btn-sm" style='float:left'>Lees meer</a>
+        </div>
         </div>`;
     });
     document.getElementById('html').innerHTML = html;
 }
+
